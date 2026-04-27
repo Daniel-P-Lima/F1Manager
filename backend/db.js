61fdbf5afc730526ca4026ea09422cc4c26a0f8e
Daniel <134326067+Daniel-P-Lima@users.noreply.github.com>
2026-04-27 10:45:42 -0300

    Remove database password from db.js
    Removed the database password for security reasons.

--- backend/db.js
+++ backend/db.js
@@ -5,7 +5,7 @@ import mysql2 from 'mysql2';
 export const db = mysql2.createConnection({
   host: 'localhost',
   user: 'root',
-  password: 'Oportunity23#',
+  password: '',
   database: 'trabalho01'
 });
 
